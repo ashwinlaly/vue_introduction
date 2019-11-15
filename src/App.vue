6b24e4ec763fa768d81fa092c1a17a16ee105895
AShwinlaly <ashwinlaly@gmail.com>
2019-11-15 16:05:02 +0530

    CRUD setup

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
-  <div id="app">
-    {{msg}}
-    <app-home-component></app-home-component>
-  </div>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="collapse navbar-collapse">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Login </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Sign up</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
-}
 </script>
+
+<style>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Home from './Home.vue';
-import Parent from './Component/Parent.vue';
-
-Vue.component('app-home-component', Home);
-Vue.component('app-Parent-Component', Parent);
 
 new Vue({
   el: '#app',
